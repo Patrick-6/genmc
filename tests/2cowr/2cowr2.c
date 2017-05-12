e1b0c2cfb328a8df8448b71e2e0c16f1d0a57481
Michalis Kokologiannakis <mixaskok@gmail.com>
2017-05-12 18:30:26 +0300

    tests: Add 2CoWR testcase

---
+++ tests/2cowr/2cowr2.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <pthread.h>
+
+int x;
+int y;
+
+void *thread_one(void *arg)
+{
+	int r = y;
+	return NULL;
+}
+
+void *thread_two(void *arg)
+{
+	int r = x;
+	return NULL;
+}
+
+void *thread_three(void *arg)
+{
+	x = 1;
+	return NULL;
+}
+
+void *thread_four(void *arg)
+{
+	y = 1;
+	return NULL;
+}
+
+int main()
+{
+	pthread_t t1, t2, t3, t4;
+
+	if (pthread_create(&t3, NULL, thread_three, NULL))
+		abort();
+	if (pthread_create(&t4, NULL, thread_four, NULL))
+		abort();
+	if (pthread_create(&t1, NULL, thread_one, NULL))
+		abort();
+	if (pthread_create(&t2, NULL, thread_two, NULL))
+		abort();
+
+	return 0;
+}
