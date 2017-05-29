e560398e555bc8f92cfc58d6604c9e1595d964fd
Michalis Kokologiannakis <mixaskok@gmail.com>
2017-05-29 16:19:18 +0300

    tests: Make explicit_store 'memory_order_release' in inc and rw testcases

--- tests/correct/rw/rw.c
+++ tests/correct/rw/rw.c
@@ -6,6 +6,6 @@ void *thread_n(void *arg)
 	int v = *((int *) arg);
 
 	atomic_load_explicit(&x, memory_order_relaxed);
-	atomic_store_explicit(&x, v, memory_order_relaxed);
+	atomic_store_explicit(&x, v, memory_order_release);
 	return NULL;
 }
