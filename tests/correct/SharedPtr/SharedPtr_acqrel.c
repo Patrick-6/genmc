d1d71b5b879c7925a5578ca90c380a5d5953ce72
Michalis Kokologiannakis <mixaskok@gmail.com>
2017-06-05 17:49:13 +0300

    tests: Fix expected results for SharedPtr testcase

--- tests/correct/SharedPtr/SharedPtr_acqrel.c
+++ tests/correct/SharedPtr/SharedPtr_acqrel.c
@@ -14,17 +14,18 @@ void *thread_zero(void *unused)
 	return NULL;
 }
 
-
 void *thread_one(void *unused)
 {
 	int c1 = 0, init;
+	atomic_int * t;
 
 	init = atomic_load_explicit(&z, memory_order_acquire);
 	__VERIFIER_assume(init == 1);
 	atomic_store_explicit(&p, &y, memory_order_release);
 	for (int i = 0; i < N; i++)
 		c1 += atomic_load_explicit(&x, memory_order_acquire);
-	atomic_store_explicit(&*p, atomic_load_explicit(&*p, memory_order_acquire) + 3,
+	t = atomic_load_explicit(&p, memory_order_acquire);
+	atomic_store_explicit(t, atomic_load_explicit(t, memory_order_acquire) + 3,
 			      memory_order_release);
 	/* assert(3 <= x && x <= 9); */
 	/* assert(3 <= y && y <= 9); */
@@ -34,13 +35,15 @@ void *thread_one(void *unused)
 void *thread_two(void *unused)
 {
 	int c2 = 0, init;
+	atomic_int * t;
 
 	init = atomic_load_explicit(&z, memory_order_acquire);
 	__VERIFIER_assume(init == 1);
 	atomic_store_explicit(&p, &x, memory_order_release);
 	for (int i = 0; i < N; i++)
 		c2 += atomic_load_explicit(&y, memory_order_acquire);
-	atomic_store_explicit(&*p, atomic_load_explicit(&*p, memory_order_acquire) + 2,
+	t = atomic_load_explicit(&p, memory_order_acquire);
+	atomic_store_explicit(t, atomic_load_explicit(t, memory_order_acquire) + 3,
 			      memory_order_release);
 	/* assert(3 <= x && x <= 9); */
 	/* assert(3 <= y && y <= 9); */
