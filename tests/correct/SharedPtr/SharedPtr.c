04b5a4c971338198d1cdb8d5d276f6d0bf04054a
Viktor Vafeiadis <viktor@mpi-sws.org>
2017-06-05 16:05:45 +0200

    Fixed SharedPtr.c

--- tests/correct/SharedPtr/SharedPtr.c
+++ tests/correct/SharedPtr/SharedPtr.c
@@ -6,11 +6,13 @@ _Atomic(atomic_int *) p;
 void *thread_one(void *unused)
 {
 	int c1 = 0;
-	
+    atomic_int * t;
+    
 	atomic_store_explicit(&p, &y, memory_order_release);
 	for (int i = 0; i < N; i++)
 		c1 += atomic_load_explicit(&x, memory_order_acquire);
-	atomic_store_explicit(&*p, atomic_load_explicit(&*p, memory_order_acquire) + 3,
+    t = atomic_load_explicit(&p, memory_order_acquire);
+    atomic_store_explicit(t, atomic_load_explicit(t, memory_order_acquire) + 3,
 			      memory_order_release);
 	/* assert(3 <= x && x <= 9); */
 	/* assert(3 <= y && y <= 9); */
@@ -20,12 +22,14 @@ void *thread_one(void *unused)
 void *thread_two(void *unused)
 {
 	int c2 = 0;
+    atomic_int * t;
 	
 	atomic_store_explicit(&p, &x, memory_order_release);
 	for (int i = 0; i < N; i++)
 		c2 += atomic_load_explicit(&y, memory_order_acquire);
-	atomic_store_explicit(&*p, atomic_load_explicit(&*p, memory_order_acquire) + 2,
-			      memory_order_release);
+    t = atomic_load_explicit(&p, memory_order_acquire);
+    atomic_store_explicit(t, atomic_load_explicit(t, memory_order_acquire) + 3,
+                          memory_order_release);
 	/* assert(3 <= x && x <= 9); */
 	/* assert(3 <= y && y <= 9); */
 	return NULL;
