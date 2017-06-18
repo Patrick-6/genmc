9918344a362506fd905ba0d307e6e9c2bb2d61ea
Michalis Kokologiannakis <mixaskok@gmail.com>
2017-06-18 20:22:44 +0300

    tests: Force peterson testcase to use C11 semantics

--- tests/cooking/peterson/peterson.c
+++ tests/cooking/peterson/peterson.c
@@ -1,76 +1,62 @@
-/* Adapted from: https://svn.sosy-lab.org/software/sv-benchmarks/trunk/c/pthread-atomic/peterson_true-unreach-call.c */
-
-#ifdef ENABLE_PSO_FENCES
-#define ENABLE_TSO_FENCES
-#endif
-
-#define FENCE() asm volatile ("mfence" ::: "memory")
-
-#ifdef ENABLE_TSO_FENCES
-#define TSO_FENCE() FENCE()
-#else
-#define TSO_FENCE() /*No FENCE*/
-#endif
-
-#ifdef ENABLE_PSO_FENCES
-#define PSO_FENCE() FENCE()
-#else
-#define PSO_FENCE() /*No FENCE*/
-#endif
-
-void __VERIFIER_assume(int);
-
 /* Testcase from Threader's distribution. For details see:
    http://www.model.in.tum.de/~popeea/research/threader
 */
 
+#include <stdlib.h>
 #include <pthread.h>
 #include <assert.h>
+#include "../../stdatomic.h"
 
-volatile int flag1 = 0, flag2 = 0; // boolean flags
-volatile int turn = 0; // integer variable to hold the ID of the thread whose turn it is
-volatile int x; // boolean variable to test mutual exclusion
+atomic_bool flag1; /* Boolean flags */
+atomic_bool flag2;
 
-void *thr1(void *arg) {
-  flag1 = 1;
-  PSO_FENCE();
-  turn = 1;
-  TSO_FENCE();
-  __VERIFIER_assume(flag2 != 1 || turn != 1);
-  // begin: critical section
-  x = 0;
-  assert(x<=0);
-  PSO_FENCE();
-  // end: critical section
-  flag1 = 0;
-  return NULL;
+atomic_int turn;   /* Atomic integer that holds the ID of the thread whose turn it is */
+atomic_bool x;     /* Boolean variable to test mutual exclusion */
+
+void __VERIFIER_assume(int);
+
+void *thread_1(void *arg)
+{
+	atomic_store_explicit(&flag1, 1, memory_order_release);
+	atomic_store_explicit(&turn, 1, memory_order_release);
+
+	__VERIFIER_assume(atomic_load_explicit(&flag2, memory_order_acquire) != 1 ||
+			  atomic_load_explicit(&turn, memory_order_acquire) != 1);
+
+	/* critical section beginning */
+	atomic_store_explicit(&x, 0, memory_order_release);
+	assert(atomic_load_explicit(&x, memory_order_acquire) <= 0);
+	/* critical section ending */
+
+	atomic_store_explicit(&flag1, 0, memory_order_release);
+	return NULL;
 }
 
-void *thr2(void *arg) {
-  flag2 = 1;
-  PSO_FENCE();
-  turn = 0;
-  TSO_FENCE();
-  __VERIFIER_assume(flag1 != 1 || turn != 0);
-  // begin: critical section
-  x = 1;
-  assert(x>=1);
-  PSO_FENCE();
-  // end: critical section
-  flag2 = 0;
-  return NULL;
+void *thread_2(void *arg)
+{
+	atomic_store_explicit(&flag2, 1, memory_order_release);
+	atomic_store_explicit(&turn, 0, memory_order_release);
+
+	__VERIFIER_assume(atomic_load_explicit(&flag1, memory_order_acquire) != 1 ||
+			  atomic_load_explicit(&turn, memory_order_acquire) != 0);
+
+	/* critical section beginning */
+	atomic_store_explicit(&x, 1, memory_order_release);
+	assert(atomic_load_explicit(&x, memory_order_acquire) >= 1);
+	/* critical section ending */
+
+	atomic_store_explicit(&flag2, 0, memory_order_release);
+	return NULL;
 }
-  
-int main() {
-  #ifndef GOTO
+
+int main()
+{
   pthread_t t1, t2;
-  pthread_create(&t1, 0, thr1, 0);
-  pthread_create(&t2, 0, thr2, 0);
-  pthread_join(t1, 0);
-  pthread_join(t2, 0);
-  #else
-  __CPROVER_ASYNC_0: thr1(NULL);
-  thr2(NULL);
-  #endif
+
+  if (pthread_create(&t1, NULL, thread_1, NULL))
+	  abort();
+  if (pthread_create(&t2, NULL, thread_2, NULL))
+	  abort();
+
   return 0;
 }
