bfac0367aa8d38e83983f395131cde8eec4fb0d3
Viktor Vafeiadis <viktor@mpi-sws.org>
2017-06-21 21:20:35 +0200

    Fixed CoA2.litmus example

--- benchmarks/compare-ocaml.sh
+++ benchmarks/compare-ocaml.sh
@@ -13,18 +13,18 @@ fi
 runtest() {
     dir=$1
     testcase="${dir%.*}"
-    herd_out=`"${HERD}" -model c11_orig.cat "${dir}"`
+    herd_out=`"${HERD}" -model rc11.cat "${dir}"`
     rcmc_out=`"${RCMC}" -mo "${testcase##*/}"`
     herd_execs=`echo "${herd_out}" | awk '/Positive/ { print $2 }'`
     rcmc_execs=`echo "${rcmc_out}" | awk '/SAFE/ { print $3 }'`
     color="${GREEN}" && [[ "${herd_execs}" != "${rcmc_execs}" ]]  && color="${RED}"
-    printf "| %-30s | %-17s | %-17s |\n" \
+    printf "| %-32s | %-17s | %-17s |\n" \
  	   "${color}${testcase##*/}${NC}" "${herd_execs}" "${rcmc_execs}"
 }
 
 # Print table's header
 printline
-printf "| %-30s | %-26s | %-26s |\n" \
+printf "| %-32s | %-28s | %-28s |\n" \
        "${CYAN}TESTCASE${NC}" "${CYAN}HERD${NC}" "${CYAN}RCMC${NC}"
 printline
 
