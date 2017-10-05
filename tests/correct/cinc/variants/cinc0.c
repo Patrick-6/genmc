d7444d6b4822305b0bcda4464519f83f95a04917
Michalis Kokologiannakis <mixaskok@gmail.com>
2017-10-05 14:56:38 +0300

    tests: Fix bug in cinc testcase
    Commit 706db940a4 forced pthread_create() to store the id of the
    created thread in its first argument, something that revealed a
    bug in the cinc testcase which led to runtime errors. This commit
    fixes this issue.

--- tests/correct/cinc/variants/cinc0.c
+++ tests/correct/cinc/variants/cinc0.c
@@ -9,7 +9,7 @@ int main()
 	pthread_t t[N];
 
 	for (int i = 0; i < N; i++)
-		if (pthread_create(&t[N], NULL, thread_n, NULL))
+		if (pthread_create(&t[i], NULL, thread_n, NULL))
 			abort();
 
 	return 0;
