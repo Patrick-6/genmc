c1b3637e146fc70d43bbe00612aa87ae067eea11
Viktor Vafeiadis <viktor@mpi-sws.org>
2019-02-09 09:47:39 +0100

    c++: add stddef.h for benchmarks

--- include/stdio.h
+++ include/stdio.h
@@ -1,6 +1,8 @@
 #ifndef __STDIO_H__
 #define __STDIO_H__
 
+#include <stddef.h>
+
 typedef __SIZE_TYPE__ size_t;
 
 struct _IO_FILE;
@@ -12,8 +14,9 @@ extern struct _IO_FILE *stderr;
 
 extern int fclose(FILE *);
 extern int fflush(FILE *);
-extern FILE *fopen(const char * restrict, const char * restrict);
-extern int fprintf(FILE * restrict, const char * restrict, ...);
-extern size_t fwrite(const void * restrict, size_t, size_t, FILE * restrict);
+extern FILE *fopen(const char *, const char *);
+extern int printf(const char *, ...);
+extern int fprintf(FILE *, const char *, ...);
+extern size_t fwrite(const void *, size_t, size_t, FILE *);
 
 #endif /* __STDIO_H__ */
