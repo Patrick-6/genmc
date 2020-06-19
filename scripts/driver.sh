dca82a82d4fac0abdb2e784eda184b0095e7dc86
Michalis Kokologiannakis <mixaskok@gmail.com>
2020-06-19 20:31:27 +0300

    tests: Suppress diff for fs tests
    Signed-off-by: Michalis Kokologiannakis <mixaskok@gmail.com>

--- scripts/driver.sh
+++ scripts/driver.sh
@@ -149,7 +149,7 @@ do
 	testdir="${wrongdir}/${cat}"
 	coherence="wb"
 	suppress_diff=""
-	if test "${cat}" = "memory"
+	if test "${cat}" = "memory" -o "${cat}" = "fs"
 	then
 	    suppress_diff=1
 	fi
