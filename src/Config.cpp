a58b78b595d69cdad51cad286c124054fbec517e
Michalis Kokologiannakis <mixaskok@gmail.com>
2020-07-13 20:55:47 +0300

    config: Refer to memory models in a consistent manner
    Signed-off-by: Michalis Kokologiannakis <mixaskok@gmail.com>

--- src/Config.cpp
+++ src/Config.cpp
@@ -46,7 +46,7 @@ clInputFile(llvm::cl::Positional, llvm::cl::Required, llvm::cl::desc("<input fil
 llvm::cl::opt<ModelType>
 clModelType(llvm::cl::values(
 		    clEnumValN(ModelType::rc11, "rc11", "RC11 memory model"),
-		    clEnumValN(ModelType::imm,  "imm",  "IMM model"),
+		    clEnumValN(ModelType::imm,  "imm",  "IMM memory model"),
 		    clEnumValN(ModelType::lkmm, "lkmm", "LKMM memory model")
 #ifdef LLVM_CL_VALUES_NEED_SENTINEL
 		    , NULL
