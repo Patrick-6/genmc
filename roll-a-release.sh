3abd75475dc8eeb0d8b7715f51794d21a7779ffe
Michalis Kokologiannakis <mixaskok@gmail.com>
2020-09-23 11:22:19 +0200

    scripts: Fix script to roll releases to github

--- roll-a-release.sh
+++ roll-a-release.sh
@@ -9,21 +9,28 @@
 #    - clean-artifacts.sh
 #    - roll-a-release.sh (the script itself)
 #
-# This script needs to be run *before* merging dev to master.
+# This script needs to be run *before* merging dev to master,
+# to master, otherwise the commit from which the patch should be
+# created needs to be passed as an argument.
 
 VERSION=0.5
 INTERNAL_PATH=/home/michalis/Documents/genmc-tool
 EXTERNAL_PATH=/home/michalis/Documents/genmc-github
 PATCH_PATH=/tmp/release-"${VERSION}".patch
+COMMIT=master
+
+[ $# -gt 0 ] && COMMIT=$1 && shift
 
 cd "${INTERNAL_PATH}" &&
-git diff master..dev --binary -- . ':!./.gitlab-ci.yml' ':!./gitlab-ci' ':!./clean-artifacts.sh' ':!./roll-a-release.sh' ':!./doc/manual.org' > "${PATCH_PATH}" &&
-git checkout master && git merge dev && git tag "v${VERSION}" && git push origin master &&
+git diff "${COMMIT}"..dev --binary -- . ':!./.gitlab-ci.yml' ':!./gitlab-ci' ':!./clean-artifacts.sh' ':!./export-github-patch.sh' ':!./roll-a-release.sh' ':!./doc/manual.org' > "${PATCH_PATH}" &&
+git checkout master && git merge dev && git tag "v${VERSION}" &&
+git push origin master && git push origin "v${VERSION}" &&
 
 cd "${EXTERNAL_PATH}" &&
 git checkout master && git apply "${PATCH_PATH}" &&
 autoreconf --install && ./configure && make ftest &&
 git add -A && git commit -s -m "Release GenMC v${VERSION}" && git tag "v${VERSION}" &&
-git push origin master && git clean -dfX &&
+git push origin master && git push origin "v${VERSION}" &&
+git clean -dfX &&
 
 echo "A new release has been rolled."
