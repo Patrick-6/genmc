49a5c7728f0f28aca5f5c2cd8e68d871235da8bc
Michalis Kokologiannakis <mixaskok@gmail.com>
2021-02-02 19:21:26 +0100

    tests: Parametrize treiber-stack
    Signed-off-by: Michalis Kokologiannakis <mixaskok@gmail.com>

--- tests/correct/data-structures/treiber-stack/main.c
+++ tests/correct/data-structures/treiber-stack/main.c
@@ -1,16 +1,39 @@
 #include "my_stack.c"
 
-#define NUM_THREADS 4
+#define MAXREADERS 3
+#define MAXWRITERS 3
+#define MAXRDWR 3
+
+#ifdef CONFIG_STACK_READERS
+#define DEFAULT_READERS (CONFIG_STACK_READERS)
+#else
+#define DEFAULT_READERS 1
+#endif
+
+#ifdef CONFIG_STACK_WRITERS
+#define DEFAULT_WRITERS (CONFIG_STACK_WRITERS)
+#else
+#define DEFAULT_WRITERS 2
+#endif
+
+#ifdef CONFIG_STACK_RDWR
+#define DEFAULT_RDWR (CONFIG_STACK_RDWR)
+#else
+#define DEFAULT_RDWR 0
+#endif
+
+int readers = DEFAULT_READERS, writers = DEFAULT_WRITERS, rdwr = DEFAULT_RDWR;
+
+static int num_threads;
 
 static mystack_t stack;
-static int num_threads = NUM_THREADS;
-static pthread_t threads[NUM_THREADS];
-static int param[NUM_THREADS];
+static pthread_t threads[MAX_THREADS];
+static int param[MAX_THREADS];
 
 unsigned int idx1, idx2;
 unsigned int a, b;
 
-atomic_int x[3];
+atomic_int x[MAX_THREADS];
 
 int __thread tid;
 
@@ -24,31 +47,47 @@ int get_thread_num()
 	return tid;
 }
 
-void *main_task(void *param)
+void *threadW(void *param)
 {
 	unsigned int val;
 	int pid = *((int *)param);
 
 	set_thread_num(pid);
 
-	if (pid % 4 == 0) {
-		atomic_store_explicit(&x[1], 17, relaxed);
-		push(&stack, 1);
-	} else if (pid % 4 == 1) {
-		atomic_store_explicit(&x[2], 37, relaxed);
-		push(&stack, 2);
-	} else if (pid % 4 == 2) {/*
-		idx1 = pop(stack);
-		if (idx1 != 0) {
-			a = atomic_load_explicit(&x[idx1], relaxed);
-			printf("a: %d\n", a);
-		}*/
-	} else {
-		idx2 = pop(&stack);
-		if (idx2 != 0) {
-			b = atomic_load_explicit(&x[idx2], relaxed);
-			/* printf("b: %d\n", b); */
-		}
+	atomic_store_explicit(&x[pid], pid + 42, relaxed);
+	push(&stack, pid);
+	return NULL;
+}
+
+void *threadR(void *param)
+{
+	unsigned int val;
+	int pid = *((int *)param);
+
+	set_thread_num(pid);
+
+	int idx = pop(&stack);
+	if (idx != 0) {
+		int b = atomic_load_explicit(&x[idx], relaxed);
+		/* printf("b: %d\n", b); */
+	}
+	return NULL;
+}
+
+void *threadRW(void *param)
+{
+	unsigned int val;
+	int pid = *((int *)param);
+
+	set_thread_num(pid);
+
+	atomic_store_explicit(&x[pid], pid + 42, relaxed);
+	push(&stack, pid);
+
+	int idx = pop(&stack);
+	if (idx != 0) {
+		int b = atomic_load_explicit(&x[idx], relaxed);
+		/* printf("b: %d\n", b); */
 	}
 	return NULL;
 }
