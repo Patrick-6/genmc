33f57ea154f77ac1eea79b3aa087d3919a233c8c
Michalis Kokologiannakis <mixaskok@gmail.com>
2022-03-01 18:42:25 +0100

    kater/driver: Pass debug value @ ctor
    Signed-off-by: Michalis Kokologiannakis <mixaskok@gmail.com>

--- kater/src/Driver.hpp
+++ kater/src/Driver.hpp
@@ -8,26 +8,29 @@
 # define YY_DECL yy::parser::symbol_type yylex (Driver& drv)
 YY_DECL;
 
-class Driver
-{
+class Driver {
+
 public:
+	Driver(bool debug = false) : variables(), acyclicity_constraints(),
+				     result(), debug(debug), file(),
+				     location() {}
 
-  std::unordered_map<std::string, std::unique_ptr<RegExp> > variables;
-  std::vector<std::unique_ptr<RegExp> > acyclicity_constraints;
+	std::unordered_map<std::string, std::unique_ptr<RegExp> > variables;
+	std::vector<std::unique_ptr<RegExp> > acyclicity_constraints;
 
-  int result;
-  bool debug;
+	int result;
+	bool debug;
 
-  // Run the parser on file F.  Return 0 on success.
-  int parse (const std::string& f);
+	// Run the parser on file F.  Return 0 on success.
+	int parse (const std::string& f);
 
-  void register_emptiness_assumption(std::unique_ptr<RegExp> e);
+	void register_emptiness_assumption(std::unique_ptr<RegExp> e);
 
-  std::string file;
+	std::string file;
 
-  void scan_begin();
-  void scan_end();
-  yy::location location;
+	void scan_begin();
+	void scan_end();
+	yy::location location;
 };
 
 #endif /* DRIVER_HPP */
