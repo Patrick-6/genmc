09da508f7ea0cfd0ec9e4cffb8c6fe83dd16cc5a
Michalis Kokologiannakis <mixaskok@gmail.com>
2022-03-07 11:08:35 +0100

    kater/nfa: Add a datatype to be used for states
    Signed-off-by: Michalis Kokologiannakis <mixaskok@gmail.com>

--- kater/src/NFA.hpp
+++ kater/src/NFA.hpp
@@ -2,7 +2,7 @@
 #define _KATER_NFA_HPP_
 
 #include "TransLabel.hpp"
-#include <functional>
+#include <algorithm>
 #include <memory>
 #include <set>
 #include <string>
@@ -28,12 +28,13 @@ private:
 		Transition(const TransLabel &lab, State *dest)
 			: label(lab), dest(dest) {}
 
-		/* Flips the label of this transition and changes its
-		 * destination to DEST */
-		Transition &flipTo(State *s) {
-			label.flip();
-			dest = s;
-			return *this;
+		/* Returns a transition with the label flipped,
+		 * and the destination changed to DEST */
+		Transition flipTo(State *s) const {
+			Transition t(*this);
+			t.label.flip();
+			t.dest = s;
+			return t;
 		}
 
 		inline bool operator==(const Transition &t) const {
@@ -55,6 +56,138 @@ private:
 		inline bool operator<=(const Transition &t) const {
 			return !(*this > t);
 		}
+	};
+
+
+	/*
+	 * A class representing NFA states.
+	 * Each state maintains a collections of its transitions.
+	 */
+	class State {
+
+	private:
+		/* FIXME: Do not use set */
+		using TransitionsC = std::set<Transition>;
+
+	public:
+		State() = delete;
+		State(unsigned id) : id(id) {}
+
+		using trans_iterator = TransitionsC::iterator;
+		using trans_const_iterator = TransitionsC::const_iterator;
+
+		trans_iterator out_begin() { return getOutgoing().begin(); }
+		trans_iterator out_end() { return getOutgoing().end(); }
+
+		trans_const_iterator out_begin() const { return getOutgoing().begin(); }
+		trans_const_iterator out_end() const { return getOutgoing().end(); }
+
+		trans_iterator in_begin() { return getIncoming().begin(); }
+		trans_iterator in_end() { return getIncoming().end(); }
+
+		trans_const_iterator in_begin() const { return getIncoming().begin(); }
+		trans_const_iterator in_end() const { return getIncoming().end(); }
+
+		/* Returns this state's ID */
+		unsigned getId() const { return id; }
+
+		/* Whether this state has an outgoing transition T */
+		bool hasOutgoing(const Transition &t) const {
+			return getOutgoing().count(t);
+		}
+
+		/* Whether this state has any outgoing transitions */
+		bool hasOutgoing() const { return out_begin() == out_end(); }
+
+		/* Whether this state has an inverse transition T */
+		bool hasIncoming(const Transition &t) const {
+			return getIncoming().count(t);
+		}
+
+		/* Whether this state has any incoming transitions */
+		bool hasIncoming() const { return in_begin() == in_end(); }
+
+		/* Adds an outgoing transition T (if it doesn't exist) */
+		void addOutgoing(const Transition &t) {
+			getOutgoing().insert(t);
+		}
+
+		/* Adds an incoming transition T (if it doesn't exist) */
+		void addIncoming(const Transition &t) {
+			getIncoming().insert(t);
+		}
+
+		/* Removes outgoing transition T from this state (if exists) */
+		void removeOutgoing(const Transition &t) {
+			getOutgoing().erase(t);
+		}
+
+		/* Removes incoming transition T from this state (if exists) */
+		void removeIncoming(const Transition &t) {
+			getIncoming().erase(t);
+		}
+
+		/* Removes all outgoing with S as their dest */
+		void removeOutgoingTo(State *s) {
+			for (auto it = out_begin(), ie = out_end(); it != ie; /* */) {
+				if (it->dest == s)
+					it = getOutgoing().erase(it);
+				else
+					++it;
+			}
+		}
+
+		/* Removes all incoming with S as their dest */
+		void removeIncomingTo(State *s) {
+			for (auto it = in_begin(), ie = in_end(); it != ie; /* */) {
+				if (it->dest == s)
+					it = getIncoming().erase(it);
+				else
+					++it;
+			}
+		}
+
+		/* Returns true if all outgoing transitions of this
+		 * state return to this state */
+		bool hasAllOutLoops() const {
+			return std::all_of(out_begin(), out_end(), [&](const Transition &t){
+				return t.dest == this;
+			});
+		}
+
+		/* Returns true if all incoming transition of this
+		 * state return to this state */
+		bool hasAllInLoops() const {
+			return std::all_of(in_begin(), in_end(), [&](const Transition &t){
+				return t.dest == this;
+			});
+		}
+
+		/* Whether all outgoing transitions are the same as the ones of S */
+		bool outgoingSameAs(State *s) const {
+			return getOutgoing() == s->getOutgoing();
+		}
+
+		/* Whether all incoming transitions are the same as the ones of S */
+		bool incomingSameAs(State *s) const {
+			return getIncoming() == s->getIncoming();
+		}
+
+		State &flip() {
+			std::swap(getOutgoing(), getIncoming());
+			return *this;
+		}
+
+	private:
+		const TransitionsC &getOutgoing() const { return transitions; }
+		TransitionsC &getOutgoing() { return transitions; }
+
+		const TransitionsC &getIncoming() const { return inverse; }
+		TransitionsC &getIncoming() { return inverse; }
+
+		unsigned id;
+		TransitionsC transitions;
+		TransitionsC inverse;
 	};
 
 public:
