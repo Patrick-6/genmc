b65376117328ec74e6c63c63f74d957e74f739f6
Michalis Kokologiannakis <mixaskok@gmail.com>
2022-03-29 15:22:06 +0200

    kater/printer: Print GenMC copyright notice in generated files
    Signed-off-by: Michalis Kokologiannakis <mixaskok@gmail.com>

--- kater/src/Printer.cpp
+++ kater/src/Printer.cpp
@@ -2,7 +2,33 @@
 #include "Error.hpp"
 #include "Printer.hpp"
 
-std::string katerNotice = "/* This file is generated automatically by Kater -- do not edit. */";
+const char *genmcCopyright =
+R"(/*
+ * GenMC -- Generic Model Checking.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, you can access it online at
+ * http://www.gnu.org/licenses/gpl-3.0.html.
+ *
+ * Author: Michalis Kokologiannakis <michalis@mpi-sws.org>
+ */
+)";
+
+const char *katerNotice =
+R"(/*******************************************************************************
+ * CAUTION: This file is generated automatically by Kater -- DO NOT EDIT.
+ *******************************************************************************/
+)";
 
 void openFileForWriting(const std::string &filename, std::ofstream &fout)
 {
@@ -84,8 +110,8 @@ void Printer::printTransLabel(const TransLabel *t, std::string res, std::string 
 
 void Printer::printHppHeader()
 {
-	*outHpp << katerNotice << "\n"
-		<< "\n";
+	*outHpp << genmcCopyright << "\n"
+		<< katerNotice << "\n";
 
 	*outHpp	<< "#ifndef " << guardName << "\n"
 		<< "#define " << guardName << "\n"
@@ -111,8 +137,8 @@ void Printer::printHppHeader()
 
 void Printer::printCppHeader()
 {
-	*outCpp << katerNotice << "\n"
-		<< "\n";
+	*outCpp << genmcCopyright << "\n"
+		<< katerNotice << "\n";
 
 	*outCpp <<  "#include \"" << className << ".hpp\"\n"
 		<< "\n";
