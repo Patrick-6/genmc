54bef32ac0157002cef99c8dfea5da6da2567286
Michalis Kokologiannakis <mixaskok@gmail.com>
2022-05-02 19:49:50 +0200

    conf,*: Do not check for old header files
    Signed-off-by: Michalis Kokologiannakis <mixaskok@gmail.com>

--- src/Interpreter.cpp
+++ src/Interpreter.cpp
@@ -42,16 +42,8 @@
 #include "Error.hpp"
 #include "Interpreter.h"
 #include <llvm/CodeGen/IntrinsicLowering.h>
-#if defined(HAVE_LLVM_IR_DERIVEDTYPES_H)
 #include <llvm/IR/DerivedTypes.h>
-#elif defined(HAVE_LLVM_DERIVEDTYPES_H)
-#include <llvm/DerivedTypes.h>
-#endif
-#if defined(HAVE_LLVM_IR_MODULE_H)
 #include <llvm/IR/Module.h>
-#elif defined(HAVE_LLVM_MODULE_H)
-#include <llvm/Module.h>
-#endif
 #include <cstring>
 
 using namespace llvm;
--- src/Interpreter.h
+++ src/Interpreter.h
@@ -55,23 +55,9 @@
 #include <llvm/ExecutionEngine/GenericValue.h>
 #include <llvm/IR/Instructions.h>
 #include <llvm/IR/DebugInfo.h>
-#if defined(HAVE_LLVM_IR_DATALAYOUT_H)
 #include <llvm/IR/DataLayout.h>
-#elif defined(HAVE_LLVM_DATALAYOUT_H)
-#include <llvm/DataLayout.h>
-#endif
-#if defined(HAVE_LLVM_IR_FUNCTION_H)
 #include <llvm/IR/Function.h>
-#elif defined(HAVE_LLVM_FUNCTION_H)
-#include <llvm/Function.h>
-#endif
-#if defined(HAVE_LLVM_INSTVISITOR_H)
-#include <llvm/InstVisitor.h>
-#elif defined(HAVE_LLVM_IR_INSTVISITOR_H)
 #include <llvm/IR/InstVisitor.h>
-#elif defined(HAVE_LLVM_SUPPORT_INSTVISITOR_H)
-#include <llvm/Support/InstVisitor.h>
-#endif
 #include <llvm/Support/DataTypes.h>
 #include <llvm/Support/ErrorHandling.h>
 #include <llvm/Support/raw_ostream.h>
